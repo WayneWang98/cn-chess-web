dfbd7d9dc966576eb8ab574ed5cd91868ab5f801
waynewang98 <1070546617@qq.com>
2021-03-21 18:04:48 +0800

    增加楚河汉界，棋盘部分完成

--- src/pages/chessBoard/index.jsx
+++ src/pages/chessBoard/index.jsx
@@ -44,9 +44,20 @@ class ChessBoard extends Component {
     const { cellWidth, offsetX, offsetY } = this
     const width = cellWidth * 8
     const height = cellWidth * 9
+    const outerWidth = cellWidth * 10
+    const outerHeight = cellWidth * 11
+
+    // 绘制外边框（必须先绘制外边框，否则填充时会覆盖内边框）
+    ctx.lineWidth = 2
+    ctx.rect(1, 1, outerWidth -2, outerHeight)
+    ctx.stroke()
+    ctx.fillStyle = '#EAC591'
+    ctx.fill()
+
+    // 绘制内边框
     ctx.rect(offsetX, offsetY, width, height)
     ctx.stroke()
-    
+
     // 画棋盘横线
     for (let i = 1; i <= 8; i ++) {
       this.drawLineInBoard(ctx, 0, cellWidth * i, cellWidth * 8, cellWidth * i)
@@ -57,14 +68,23 @@ class ChessBoard extends Component {
       this.drawLineInBoard(ctx, i * cellWidth, 0, i * cellWidth, cellWidth * 4) // 黑方竖线
       this.drawLineInBoard(ctx, i * cellWidth, 5 * cellWidth, i * cellWidth, cellWidth * 9) // 红方竖线
     }
+
+    // 绘制楚河汉界
+    ctx.font = 'bold 30px Courier New'
+    ctx.fillStyle = '#000'
+    ctx.fillText('楚  河', cellWidth * 2, cellWidth * 5 + cellWidth / 2 + 10)
+    ctx.fillText('漢  界', cellWidth * 6, cellWidth * 5 + cellWidth / 2 + 10)
   }
 
   // 画简单直线
   drawLineInBoard (ctx, x1, y1, x2, y2) { // 参数中的坐标相对棋盘定位，而不是相对画布定位
     const { offsetX, offsetY } = this
+    ctx.beginPath()
     ctx.moveTo(x1 + offsetX, y1 + offsetY)
     ctx.lineTo(x2 + offsetX, y2 + offsetY)
+    ctx.closePath()
     ctx.stroke()
+    
   }
 
   // 绘制“井”（兵林线和布置线上的点）
@@ -114,6 +134,7 @@ class ChessBoard extends Component {
   // 绘制九宫
   drawPalaces (ctx) {
     const { cellWidth } = this
+
     this.drawLineInBoard(ctx, 3 * cellWidth, 0, 5 * cellWidth, 2 * cellWidth)
     this.drawLineInBoard(ctx, 3 * cellWidth, 2 * cellWidth, 5 * cellWidth, 0)
     this.drawLineInBoard(ctx, 3 * cellWidth, 7 * cellWidth, 5 * cellWidth, 9 * cellWidth)
